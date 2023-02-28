948d619e08f8381f9d975768eef341162404441e
Enzifiri <76253089+enzifiri@users.noreply.github.com>
2023-02-28 22:34:50 +0300

    Create deneme.sh

---
+++ cosmos-scripts/deneme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/common.sh)
+
+printLogo
+
+read -r -p "Enter node moniker: " NODE_MONIKER
